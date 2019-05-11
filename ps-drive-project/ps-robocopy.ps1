9e9474c848030b5e6c6128814750752178895d79
Ethan Motion <ethan.motion@gmail.com>
2019-05-12 11:29:29 +1200

    Update ps-robocopy.ps1

--- ps-drive-project/ps-robocopy.ps1
+++ ps-drive-project/ps-robocopy.ps1
@@ -1,79 +1,45 @@
-﻿# Roboxopy docs: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/robocopy
-# Powershell <-> XML docs: http://www.tomsitpro.com/articles/powershell-read-xml-files,2-895.html
+# Robocopy docs: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/robocopy
+# Powershell <-> XML docs: https://www.business.com/articles/powershell-read-xml-files/
 
 function main {
-    Confirm-Backup
+    backup-files
 }
 
-function Confirm-Backup {
-    $posAnswers = "Yes","Yep","Ya","Yaa","Yaas","Yep","Yup","Yip","Y","Ye","Yars","Yea","Yeah","Yaas","Yeh","Yah"
-    $negAnswers = "No","Na","Nah","Nop","Nope","Nup","N","Neg","Newp"
-    $counter = 0
-    Do{
-        Write-host "Backup files?" -ForegroundColor Yellow 
-        $answer = Read-Host 
-        if ($answer -in $posAnswers){
-            backup-files('yes')
-        }
-        elseif ($answer -in $negAnswers){
-            backup-files('no')
-        }
-        # Invalid response will cause exit
-        else {
-            #write-host "Counter =" $counter
-            Write-Host "Yes or no" -ForegroundColor Red }
-        }
-    } While ($counter -le 3)
-}
+function backup-files{
+    write-host "Backing up..." -ForegroundColor Yellow
 
-function backup-files($confirmation){
-    if($confirmation -eq 'yes'){
-        write-host "Backing up..." -ForegroundColor Yellow
-        
-        md -Force D:\Backups
-        
-        # $env:USERPROFILE
-        $xmlConfig = [XML](Get-Content .\config.xml)
-        $sources = $xmlConfig.Config.Source
-        $datedFolder = $xmlConfig.Config.DestinationDirectory + '\' + ((Get-Date).ToString('yyyy-MM-dd'))
+    $xmlConfig = [XML](Get-Content .\config.xml)
+    $sources = $xmlConfig.Config.Source
+    md -Force $xmlConfig.Config.DestinationDirectory
+    $datedFolder = $xmlConfig.Config.DestinationDirectory + '\' + ((Get-Date).ToString('yyyy-MM-dd'))
+    New-Item -ItemType Directory -Path $datedFolder
 
-        New-Item -ItemType Directory -Path $datedFolder
-
-        foreach ($Source in $sources){
-            Try {
-                $destDirName = $datedFolder + '\' + $Source.DestinationName
-                New-Item -ItemType Directory -Path $destDirName
-                $SourceDir = $Source.SourceDirectory
-                $exDirectories = $Source.ExcludeDirectories.Dir
-                $exFiles = $Source.ExcludeFiles.File
-                # Create the logfile name
-                $Logfile = $destDirName + '\backup' + '.log'
-                Start-Transcript $Logfile -NoClobber
-                Robocopy $SourceDir $destDirName /MIR /256 /xd $exDirectories /xf $exFiles /r:1 /w:1
-                Stop-Transcript
-            }
-            Catch {
+    foreach ($Source in $sources){
+        Try {
+            $destDirName = $datedFolder + '\' + $Source.DestinationName
+            New-Item -ItemType Directory -Path $destDirName
+            $SourceDir = $Source.SourceDirectory
+            $exDirectories = $Source.ExcludeDirectories.Dir
+            $exFiles = $Source.ExcludeFiles.File
+            # Create the logfile name
+            $Logfile = $destDirName + '\backup' + '.log'
+            Start-Transcript $Logfile -NoClobber
+            Robocopy $SourceDir $destDirName /MIR /256 /xd $exDirectories /xf $exFiles /r:1 /w:1
+            Stop-Transcript
+        } Catch {
             Write-Host "An error occurred:" $PSItem -ForegroundColor Red
-            }
         }
-        write-Host ""
-        write-Host "Done!"
-        write-Host ""
-        Start-Sleep -s 2
     }
-    # Don't want to backup
-    else {
-        Write-Host "Not backing up" -ForegroundColor Yellow
-        # Do nothing
-    }
-    Read-Host "Press any key to exit..."
-    exit
+    write-Host ""
+    write-Host "Done!" -ForegroundColor Green
+    write-Host ""
+    Start-Sleep -s 2
 }
 
-# Run the main function
 main
 
-#XML needs:
+# Possible improvements:
 #	Default destination folder
 #	Retention days (delete if more than X days old)
-#	Backup XML config file too? Yes.
+#	Back up the XML config file too
+# Zip the backed-up folder
